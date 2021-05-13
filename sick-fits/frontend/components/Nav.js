1c8ac7d60ee02b70457f5f48d714d642e2bff33c
Guide Fari <guideg6@gmail.com>
2021-05-13 10:58:57 +0200

    fixing & styling the nav

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,12 +1,13 @@
 import Link from 'next/link';
+import NavStles from './styles/NavStyles';
 
 export default function Nav() {
   return (
-    <nav>
+    <NavStles>
       <Link href="/products">Products</Link>
       <Link href="/sell">Sell</Link>
       <Link href="/orders">Orders</Link>
       <Link href="/account">Account</Link>
-    </nav>
+    </NavStles>
   );
 }
