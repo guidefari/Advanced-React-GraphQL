41a6cd794878fc6092ee412ad466429b86b9dcde
Guide Fari <guideg6@gmail.com>
2021-07-09 01:07:09 +0200

    cart - using react context for our cart state

---
+++ sick-fits/frontend/lib/cartState.js
@@ -0,0 +1,36 @@
+import { useState, useContext, createContext } from 'react';
+
+const LocalStateContext = createContext();
+const LocalStateProvider = LocalStateContext.Provider;
+
+function CartStateProvider({ children }) {
+  // this is our own custom provider 😎we will store data(state) and functionality(updaters) in here and anyone can access it via the consumer!
+
+  const [cartOpen, setCartOpen] = useState(false);
+
+  function toggleCart() {
+    setCartOpen(!cartOpen);
+  }
+  function closeCart() {
+    setCartOpen(false);
+  }
+  function openCart() {
+    setCartOpen(true);
+  }
+
+  return (
+    <LocalStateProvider
+      value={{ cartOpen, setCartOpen, toggleCart, closeCart, openCart }}
+    >
+      {children}
+    </LocalStateProvider>
+  );
+}
+
+// make a custom hook for accessing the cart local state
+function useCart() {
+  // we use a consumer here to access the local state
+  const all = useContext(LocalStateContext);
+  return all;
+}
+export { CartStateProvider, useCart };
