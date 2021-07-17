bed9a4cfb8c88b99ec61b10fe647d9f2c0686c40
Guide Fari <guideg6@gmail.com>
2021-07-17 17:07:06 +0200

    backend: permissions and access functions

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,7 +1,26 @@
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
+// At it's simplest, the access control returns a yes or no value depending on the users session
 
-// At its simplest, access control returns a yes or a no value depending on the users session
-
-export function isSignedIn({ session }: ListAccessArgs): boolean {
+export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// check if someone meets a criteria - yes or no
+export const permissions = {
+  ...generatedPermissions,
+  isAwesome({ session }: ListAccessArgs): boolean {
+    return session?.data.name.includes('guide');
+  },
+};
+
+// rule based functions
