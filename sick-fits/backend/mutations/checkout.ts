e14d30de591b598c0e55433448182d9f028bc17b
Guide Fari <guideg6@gmail.com>
2021-07-17 10:49:14 +0200

    debugging undefined cartItems

--- sick-fits/backend/mutations/checkout.ts
+++ sick-fits/backend/mutations/checkout.ts
@@ -61,6 +61,30 @@ export default async function checkout(root: any,{ token }: Arguments ,context: 
   })
   console.log(charge)
   // 4. Convert the cartItems to OrderItems
-  // 5. Create the order and return it
+  const orderItems = cartItems.map(cartItem => {
+      const orderItem = {
+          name: cartItem.product.name,
+          description: cartItem.product.description,
+          price: cartItem.product.price,
+          quantity: cartItem.quantity,
+          photo: {connect: {id: cartItem.product.photo.id}},
+        }
+        return orderItem
+    })
+    // 5. Create the order and return it
+    const order = await context.lists.Order.createOne({
+        data: {
+            total: charge.amount,
+            charge: charge.id,
+            items: {create: orderItems},
+            user: {connect: {id: userId}}
+      }
+  })
+//   6. clean up any old cart item
+const cartItemIds = cartItems.map(cartItem => cartItem.id)
+await context.lists.cartItem.deleteMany({
+    ids: cartItemIds
+})
+return order
 
 }
