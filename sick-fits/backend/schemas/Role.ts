c9ba05b70a40e6195080e3ae1fd5887c06dbd7ea
Guide Fari <guideg6@gmail.com>
2021-07-18 09:46:37 +0200

    backend: getting meta - roles based roles & hiding ui

--- sick-fits/backend/schemas/Role.ts
+++ sick-fits/backend/schemas/Role.ts
@@ -1,8 +1,22 @@
 import { relationship, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { permissions } from '../access';
 import { permissionFields } from './fields';
 
 export const Role = list({
+  // first limit access control
+  access: {
+    create: permissions.canManageRoles,
+    read: permissions.canManageRoles,
+    update: permissions.canManageRoles,
+    delete: permissions.canManageRoles,
+  },
+  // then you hide the ui from them. hover over hideCreate & hideDelete for explanation
+  ui: {
+    hideCreate: (args) => !permissions.canManageRoles(args),
+    hideDelete: (args) => !permissions.canManageRoles(args),
+    isHidden: (args) => !permissions.canManageRoles(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     ...permissionFields,
